1ca3d7052914d0d0e1380cc1df03070ef927054e
Faustino Henriques <71952910+f50kdev@users.noreply.github.com>
2023-03-24 23:48:30 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+console.log('Seja benvindo')
+var n1 = 34;
+soma = n1*3;
+ console.log(soma)
---
+++ texte.js
@@ -0,0 +1,8 @@
+console.log('seja benvindo')
+
+var inteiro ;
+var nome;
+const Pdenty = 1010;
+ nome = 'faustino';
+alert(nome)
+console.log(nome == nome)
